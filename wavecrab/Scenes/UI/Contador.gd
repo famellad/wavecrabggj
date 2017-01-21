ac279bbf7b08ba0d938900039e4b135e2b362ce8
zardoru <forsakengmex@gmail.com>
2017-01-21 10:56:40 -0300

    Advertencia y flecha

--- wavecrab/Scenes/UI/Contador.gd
+++ wavecrab/Scenes/UI/Contador.gd
@@ -3,5 +3,8 @@ extends Node2D
 var valor = 0
 onready var _label = get_node("Label")
 
+func _ready():
+	set_process(true)
+
 func _process(delta):
 	_label.set_text(str(valor))
---
+++ wavecrab/Scenes/UI/warn.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var dir_flecha = Vector2(0, 0)
+onready var flecha = get_node("flecha")
+onready var dist_flecha = flecha.get_pos().length()
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	
+	flecha.set_pos(dir_flecha * dist_flecha)
+	flecha.set_rot(-atan2(dir_flecha.x, -dir_flecha.y))
