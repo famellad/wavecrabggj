3523c317a181b3aa70d8e1a0a505bd7a43cf88d8
zardoru <forsakengmex@gmail.com>
2017-01-21 18:11:54 -0300

    Castillo, bump recursos

--- wavecrab/Scenes/UI/ContadorRecursos.tres.gd
+++ wavecrab/Scenes/UI/ContadorRecursos.tres.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
 onready var _label = get_node("Label")
+onready var _anim = get_node("Label/Anim")
 var valor setget set_label, get_label
 
 func set_label(s):
+	var val = int(s)
+	
+	if val < int(_label.get_text()):
+		_anim.play("Dec")
+	else:
+		_anim.play("Inc")
+		
 	_label.set_text(s)
 	
 func get_label():
