8384cd295ab56a15cb60bdf832e0923aef444df0
zardoru <forsakengmex@gmail.com>
2017-01-21 18:01:35 -0300

    BUMP

--- wavecrab/Scenes/UI/Contador.gd
+++ wavecrab/Scenes/UI/Contador.gd
@@ -3,11 +3,14 @@ extends Node2D
 var valor = 0
 
 onready var _label = get_node("Label")
-
+onready var _anim = get_node("pentagono/Anim")
 func _ready():
 	set_process(true)
 
 
 		
 func set_value(val):
+	if (str(val) != _label.get_text()):
+		_anim.play("BumpTime")
+	
 	_label.set_text(str(val))
