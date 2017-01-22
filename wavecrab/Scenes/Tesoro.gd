231070e806276b56edac96d715acaadeeda7abfc
NaxoOo <ieastudi@uc.cl>
2017-01-22 12:56:55 -0300

    Blah

--- wavecrab/Scenes/Tesoro.gd
+++ wavecrab/Scenes/Tesoro.gd
@@ -7,6 +7,8 @@ onready var animt = get_node("Tartarugas/AnimTart")
 
 func _ready():
 	get_node("Tartarugas").set_opacity( 0 )
+	get_parent().get_node("GUI/game_over").set_opacity( 0 )
+	get_parent().get_node("GUI/again").set_opacity( 0 )
 	set_fixed_process(true)
 
 func _fixed_process(delta):
@@ -29,7 +31,11 @@ func _fixed_process(delta):
 			new_cam.make_current()
 			#Problema! Como sólo se mueve la animación, la cámara no sigue al objeto... Cómo se puede arreglar?
 			get_node("game_over").show()
+			#var game_over = get_parent().get_node("GUI/game_over")
+			#game_over.set_pos(Vector2(game_over.get_pos().x, game_over.get_pos().y + 1080))
 			get_node("again").show()
+			#var again = get_parent().get_node("GUI/again")
+			#again.set_pos(Vector2(again.get_pos().x, again.get_pos().y + 1080))
 			
 			
 func add_tartaruga( tartaruga ):
